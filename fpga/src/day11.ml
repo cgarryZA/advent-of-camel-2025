42043aafdb1109ec511c858baef4aa546901d303
cgarryZA <christiangarry.southafrica@gmail.com>
2026-01-12 21:35:17 +0000

    Done

--- fpga/src/day11.ml
+++ fpga/src/day11.ml
@@ -6,15 +6,16 @@ open! Signal
 open! Hardcaml.Always
 
 let clock_freq = Ulx3s.Clock_freq.Clock_25mhz
-let uart_fifo_depth = 64
+
+let uart_fifo_depth  = 64
 let extra_synth_args = []
 
 (* ====================== RAM ====================== *)
 
 module Ram = Loadable_pseudo_dual_port_ram.Make (struct
-  let width = 64
-  let depth = 16384
-  let num_ports = 2
+  let width           = 64
+  let depth           = 16384
+  let num_ports       = 2
   let zero_on_startup = false
 end)
 
@@ -23,9 +24,9 @@ end)
 module Loader = struct
   module I = struct
     type 'a t =
-      { clock : 'a
-      ; clear : 'a
-      ; uart_rx : 'a Uart.Byte_with_valid.t
+      { clock    : 'a
+      ; clear    : 'a
+      ; uart_rx  : 'a Uart.Byte_with_valid.t
       ; uart_rts : 'a
       }
     [@@deriving hardcaml]
@@ -34,8 +35,8 @@ module Loader = struct
   module O = struct
     type 'a t =
       { load_finished : 'a
-      ; ram_write : 'a Ram.Port.t
-      ; data_words : 'a [@bits 14]
+      ; ram_write     : 'a Ram.Port.t
+      ; data_words    : 'a [@bits 14]
       ; uart_rx_ready : 'a
       }
     [@@deriving hardcaml]
@@ -52,7 +53,7 @@ module Loader = struct
       reg_fb spec ~width:14 ~enable:word_in.valid ~f:(fun x -> x +:. 1)
     in
 
-    let loaded = Variable.reg spec ~width:1 in
+    let loaded     = Variable.reg spec ~width:1 in
     let data_words = Variable.reg spec ~width:14 in
 
     (* If RTS comes on the same cycle as the final completed word, include it. *)
@@ -60,7 +61,7 @@ module Loader = struct
 
     compile
       [ when_ uart_rts
-          [ loaded <-- vdd
+          [ loaded     <-- vdd
           ; data_words <-- word_count_written
           ]
       ]
@@ -69,11 +70,11 @@ module Loader = struct
     { O.
       load_finished = loaded.value
     ; ram_write =
-        { address = word_count
-        ; write_data = word_in.value
+        { address      = word_count
+        ; write_data   = word_in.value
         ; write_enable = word_in.valid
         }
-    ; data_words = data_words.value
+    ; data_words    = data_words.value
     ; uart_rx_ready = vdd
     }
   ;;
@@ -89,12 +90,10 @@ end
 module States = struct
   type t =
     | Loading
-
     | Header0_read
     | Header0_consume
     | Header1_read
     | Header1_consume
-
     | Run_setup
     | Reset_read
     | Reset_write
@@ -111,29 +110,64 @@ module States = struct
   [@@deriving enumerate, sexp_of, compare ~localize]
 end
 
+type header_vars =
+  { num_nodes :  Variable.t
+  ; you       :  Variable.t
+  ; out_      :  Variable.t
+  ; svr       :  Variable.t
+  ; fft       :  Variable.t
+  ; dac       :  Variable.t
+  ; has_p2    :  Variable.t
+  }
+
 let algo ~clock ~clear ~(read_data : Signal.t array) ~load_finished =
   let spec = Reg_spec.create ~clock ~clear () in
+
   let sm = State_machine.create (module States) spec in
 
   let header_words = 2 in
-  let node_base = header_words in
+  let node_base    = header_words in
 
-  let ram_addr  = Variable.wire ~default:(zero 14) () in
+  let write_en   = Variable.wire ~default:gnd () in
+  let ram_addr   = Variable.wire ~default:(zero 14) () in
   let write_addr = Variable.wire ~default:(zero 14) () in
   let write_data = Variable.wire ~default:(zero 64) () in
-  let write_en   = Variable.wire ~default:gnd () in
 
   (* Read from port 0 (addressed by ram_addr in top) *)
   let read_word = read_data.(0) in
 
-  (* Header *)
-  let num_nodes = Variable.reg spec ~width:14 in
-  let idx_you = Variable.reg spec ~width:14 in
-  let idx_out = Variable.reg spec ~width:14 in
-  let idx_svr = Variable.reg spec ~width:14 in
-  let idx_fft = Variable.reg spec ~width:14 in
-  let idx_dac = Variable.reg spec ~width:14 in
-  let has_part2 = Variable.reg spec ~width:1 in
+  (* ---------- Helpers ---------- *)
+
+  let to64 x = uresize ~width:64 x in
+
+  let mul_lo64 a b =
+    let p = a *: b in
+    select p ~high:63 ~low:0
+  in
+
+  let or_reduce (x : Signal.t) =
+    match bits_lsb x with
+    | []    -> gnd
+    | [ b ] -> b
+    | bs    -> List.reduce_exn bs ~f:(|:)
+  in
+
+  let mux_arr (sel : Signal.t) (arr : Signal.t array) =
+    mux sel (Array.to_list arr)
+  in
+
+  (* ---------- Header regs bundled ---------- *)
+
+  let hdr : header_vars =
+    { num_nodes = Variable.reg spec ~width:14
+    ; you       = Variable.reg spec ~width:14
+    ; out_      = Variable.reg spec ~width:14
+    ; svr       = Variable.reg spec ~width:14
+    ; fft       = Variable.reg spec ~width:14
+    ; dac       = Variable.reg spec ~width:14
+    ; has_p2    = Variable.reg spec ~width:1
+    }
+  in
 
   (* Runs:
      0: you->out
@@ -143,29 +177,58 @@ let algo ~clock ~clear ~(read_data : Signal.t array) ~load_finished =
      4: svr->dac
      5: dac->fft
      6: fft->out *)
-  let run_id = Variable.reg spec ~width:3 in
-  let start_idx = Variable.reg spec ~width:14 in
+  let run_id     = Variable.reg spec ~width:3  in
+  let start_idx  = Variable.reg spec ~width:14 in
   let target_idx = Variable.reg spec ~width:14 in
+
+  let run_start =
+    [| hdr.you.value
+     ; hdr.svr.value
+     ; hdr.fft.value
+     ; hdr.dac.value
+     ; hdr.svr.value
+     ; hdr.dac.value
+     ; hdr.fft.value
+     ; hdr.you.value
+    |]
+  in
+
+  let run_target =
+    [| hdr.out_.value
+     ; hdr.fft.value
+     ; hdr.dac.value
+     ; hdr.out_.value
+     ; hdr.dac.value
+     ; hdr.fft.value
+     ; hdr.out_.value
+     ; hdr.out_.value
+    |]
+  in
+
+  (* last_run = 0 when no part2, else 6 *)
+  let last_run = mux2 hdr.has_p2.value (of_int_trunc ~width:3 6) (zero 3) in
 
   (* Iterators *)
   let reset_idx = Variable.reg spec ~width:14 in
-  let node_idx = Variable.reg spec ~width:14 in
-  let edge_idx = Variable.reg spec ~width:10 in
+  let node_idx  = Variable.reg spec ~width:14 in
+  let edge_idx  = Variable.reg spec ~width:10 in
 
   (* Latches *)
-  let parent_count = Variable.reg spec ~width:32 in
-  let curr_edge_base = Variable.reg spec ~width:14 in
+  let parent_count    = Variable.reg spec ~width:32 in
+  let curr_edge_base  = Variable.reg spec ~width:14 in
   let curr_edge_count = Variable.reg spec ~width:10 in
-  let child_idx = Variable.reg spec ~width:14 in
+  let child_idx       = Variable.reg spec ~width:14 in
 
-  (* Captures *)
-  let p_you_out = Variable.reg spec ~width:32 in
-  let p_svr_fft = Variable.reg spec ~width:32 in
-  let p_fft_dac = Variable.reg spec ~width:32 in
-  let p_dac_out = Variable.reg spec ~width:32 in
-  let p_svr_dac = Variable.reg spec ~width:32 in
-  let p_dac_fft = Variable.reg spec ~width:32 in
-  let p_fft_out = Variable.reg spec ~width:32 in
+  (* Captures (paths for each run_id 0..6) *)
+  let captures = Array.init 7 ~f:(fun _ -> Variable.reg spec ~width:32) in
+
+  let p_you_out = captures.(0) in
+  let p_svr_fft = captures.(1) in
+  let p_fft_dac = captures.(2) in
+  let p_dac_out = captures.(3) in
+  let p_svr_dac = captures.(4) in
+  let p_dac_fft = captures.(5) in
+  let p_fft_out = captures.(6) in
 
   let part1 = Variable.reg spec ~width:64 in
   let part2 = Variable.reg spec ~width:64 in
@@ -177,18 +240,10 @@ let algo ~clock ~clear ~(read_data : Signal.t array) ~load_finished =
      [63:32] count (u32)
      [31:18] edge_base (u14) absolute RAM addr
      [17:8 ] edge_count (u10)
-     [7:0  ] flags (u8)
-  *)
-  let node_count w = select w ~high:63 ~low:32 in
-  let node_edge_base w = select w ~high:31 ~low:18 in
+     [7:0  ] flags (u8) *)
+  let node_count w      = select w ~high:63 ~low:32 in
+  let node_edge_base w  = select w ~high:31 ~low:18 in
   let node_edge_count w = select w ~high:17 ~low:8 in
-
-  let to64 x = uresize ~width:64 x in
-
-  let mul_lo64 a b =
-    let p = a *: b in
-    select p ~high:63 ~low:0
-  in
 
   let term_a =
     mul_lo64
@@ -202,11 +257,20 @@ let algo ~clock ~clear ~(read_data : Signal.t array) ~load_finished =
   in
   let part2_calc = term_a +: term_b in
 
-  let or_reduce (x : Signal.t) =
-    match bits_lsb x with
-    | [] -> gnd
-    | [b] -> b
-    | bs -> List.reduce_exn bs ~f:(|:)
+  (* Small local proc to keep Node_consume readable *)
+  let do_node_dp () =
+    proc
+      [ parent_count    <-- node_count read_word
+      ; curr_edge_base  <-- node_edge_base read_word
+      ; curr_edge_count <-- node_edge_count read_word
+      ; if_ ((node_count read_word ==:. 0) |: (node_edge_count read_word ==:. 0))
+          [ node_idx <-- node_idx.value +:. 1
+          ; sm.set_next Node_read
+          ]
+          [ edge_idx <--. 0
+          ; sm.set_next Edge_read
+          ]
+      ]
   in
 
   compile
@@ -226,13 +290,12 @@ let algo ~clock ~clear ~(read_data : Signal.t array) ~load_finished =
           )
 
         ; ( Header0_consume
-          , [ num_nodes <-- select read_word ~high:13 ~low:0
-            ; idx_you   <-- select read_word ~high:27 ~low:14
-            ; idx_out   <-- select read_word ~high:41 ~low:28
-            ; idx_svr   <-- select read_word ~high:55 ~low:42
+          , [ hdr.you       <-- select read_word ~high:27 ~low:14
+            ; hdr.svr       <-- select read_word ~high:55 ~low:42
+            ; hdr.out_      <-- select read_word ~high:41 ~low:28
+            ; hdr.num_nodes <-- select read_word ~high:13 ~low:0
 
-            ; has_part2 <-- or_reduce (select read_word ~high:63 ~low:56)
-
+            ; hdr.has_p2    <-- or_reduce (select read_word ~high:63 ~low:56)
             ; sm.set_next Header1_read
             ]
           )
@@ -244,44 +307,24 @@ let algo ~clock ~clear ~(read_data : Signal.t array) ~load_finished =
           )
 
         ; ( Header1_consume
-          , [ idx_fft <-- select read_word ~high:13 ~low:0
-            ; idx_dac <-- select read_word ~high:27 ~low:14
-
-            ; has_part2 <-- (has_part2.value |: or_reduce (select read_word ~high:63 ~low:28))
-
-            ; run_id  <--. 0
+          , [ run_id     <--. 0
+            ; hdr.fft    <-- select read_word ~high:13 ~low:0
+            ; hdr.dac    <-- select read_word ~high:27 ~low:14
+            ; hdr.has_p2 <-- (hdr.has_p2.value |: or_reduce (select read_word ~high:63 ~low:28))
             ; sm.set_next Run_setup
             ]
           )
 
         ; ( Run_setup
-          , [ start_idx <--
-                mux run_id.value
-                  [ idx_you.value
-                  ; idx_svr.value
-                  ; idx_fft.value
-                  ; idx_dac.value
-                  ; idx_svr.value
-                  ; idx_dac.value
-                  ; idx_fft.value
-                  ; idx_you.value ]
-            ; target_idx <--
-                mux run_id.value
-                  [ idx_out.value
-                  ; idx_fft.value
-                  ; idx_dac.value
-                  ; idx_out.value
-                  ; idx_dac.value
-                  ; idx_fft.value
-                  ; idx_out.value
-                  ; idx_out.value ]
-            ; reset_idx <--. 0
+          , [ start_idx  <-- mux_arr run_id.value run_start
+            ; reset_idx  <--. 0
+            ; target_idx <-- mux_arr run_id.value run_target
             ; sm.set_next Reset_read
             ]
           )
 
         ; ( Reset_read
-          , [ if_ (reset_idx.value ==: num_nodes.value)
+          , [ if_ (reset_idx.value ==: hdr.num_nodes.value)
                 [ (* traverse all nodes; start node is the only one seeded to 1 *)
                   node_idx <--. 0
                 ; sm.set_next Node_read
@@ -299,14 +342,14 @@ let algo ~clock ~clear ~(read_data : Signal.t array) ~load_finished =
                   [ mux2 (reset_idx.value ==: start_idx.value) (one 32) (zero 32)
                   ; select read_word ~high:31 ~low:0
                   ]
-            ; write_en <-- vdd
+            ; write_en  <-- vdd
             ; reset_idx <-- reset_idx.value +:. 1
             ; sm.set_next Reset_read
             ]
           )
 
         ; ( Node_read
-          , [ if_ (node_idx.value ==: num_nodes.value)
+          , [ if_ (node_idx.value ==: hdr.num_nodes.value)
                 [ sm.set_next Capture_addr ]
                 [ ram_addr <-- uresize ~width:14 (node_idx.value +:. node_base)
                 ; sm.set_next Node_consume
@@ -315,17 +358,7 @@ let algo ~clock ~clear ~(read_data : Signal.t array) ~load_finished =
           )
 
         ; ( Node_consume
-          , [ parent_count    <-- node_count read_word
-            ; curr_edge_base  <-- node_edge_base read_word
-            ; curr_edge_count <-- node_edge_count read_word
-            ; if_ ((node_count read_word ==:. 0) |: (node_edge_count read_word ==:. 0))
-                [ node_idx <-- node_idx.value +:. 1
-                ; sm.set_next Node_read
-                ]
-                [ edge_idx <--. 0
-                ; sm.set_next Edge_read
-                ]
-            ]
+          , [ do_node_dp () ]
           )
 
         ; ( Edge_read
@@ -376,28 +409,22 @@ let algo ~clock ~clear ~(read_data : Signal.t array) ~load_finished =
           )
 
         ; ( Capture_consume
-          , [ when_ (run_id.value ==:. 0) [ p_you_out <-- node_count read_word ]
-            ; when_ (run_id.value ==:. 1) [ p_svr_fft <-- node_count read_word ]
-            ; when_ (run_id.value ==:. 2) [ p_fft_dac <-- node_count read_word ]
-            ; when_ (run_id.value ==:. 3) [ p_dac_out <-- node_count read_word ]
-            ; when_ (run_id.value ==:. 4) [ p_svr_dac <-- node_count read_word ]
-            ; when_ (run_id.value ==:. 5) [ p_dac_fft <-- node_count read_word ]
-            ; when_ (run_id.value ==:. 6) [ p_fft_out <-- node_count read_word ]
-
-            ; if_ (run_id.value ==:. 0 &: ~:(has_part2.value))
+          , [ (* Write capture for the current run_id *)
+              proc
+                (List.init 7 ~f:(fun i ->
+                     when_ (run_id.value ==:. i)
+                       [ captures.(i) <-- node_count read_word ]))
+            ; if_ (run_id.value ==: last_run)
                 [ sm.set_next Final_compute ]
-                [ if_ (run_id.value ==:. 6)
-                    [ sm.set_next Final_compute ]
-                    [ run_id <-- run_id.value +:. 1
-                    ; sm.set_next Run_setup
-                    ]
+                [ run_id <-- run_id.value +:. 1
+                ; sm.set_next Run_setup
                 ]
             ]
           )
 
         ; ( Final_compute
           , [ part1 <-- to64 p_you_out.value
-            ; part2 <-- mux2 has_part2.value part2_calc (zero 64)
+            ; part2 <-- mux2 hdr.has_p2.value part2_calc (zero 64)
             ; sm.set_next Done
             ]
           )
