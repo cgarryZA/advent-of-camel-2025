7532e909b979aa8875876f1a01bc195487dbc2e9
cgarryZA <christiangarry.southafrica@gmail.com>
2026-01-13 18:58:03 +0000

    stage 16a working

--- fpga/src/day08.ml
+++ fpga/src/day08.ml
@@ -72,24 +72,37 @@ module Loader = struct
   ;;
 end
 
-(* ====================== STAGE 12 ALGO ====================== *)
-(* All-pairs squared Euclidean distance
-   Part 1 = min d2
-   Part 2 = max d2
+(* ====================== STAGE 15 ALGO ====================== *)
+(* Top-10 edges by squared Euclidean distance, then apply them (Kruskal prefix).
+   Part 1 = product of the sizes of the three largest circuits after 10 edges
+   Part 2 = number of successful unions among those 10 edges
 *)
 
 module States = struct
   type t =
-    | Loading | Init
+    | Loading
+    | Init_scan
+    (* scan pairs -> build top-10 edge list *)
     | Addr_ax | Wait_ax | Consume_ax
     | Addr_ay | Wait_ay | Consume_ay
     | Addr_az | Wait_az | Consume_az
     | Addr_bx | Wait_bx | Consume_bx
     | Addr_by | Wait_by | Consume_by
     | Addr_bz | Wait_bz | Consume_bz
-    | Compute
+    | Compute_edge
     | Advance_j
     | Advance_i
+    (* init union-find *)
+    | Init_uf
+    (* apply edges 0..9 *)
+    | Apply_load
+    | FindU_step
+    | FindV_step
+    | Do_union
+    | Next_edge
+    (* sweep roots to find top3 sizes *)
+    | Sweep_init
+    | Sweep_step
     | Done
   [@@deriving enumerate, sexp_of, compare ~localize]
 end
@@ -101,6 +114,8 @@ let algo ~clock ~clear ~load_finished ~(word_count : Signal.t) ~(read_word : Sig
   let addr   = Variable.reg spec ~width:14 in
   let base_i = Variable.reg spec ~width:14 in
   let base_j = Variable.reg spec ~width:14 in
+  let i_idx  = Variable.reg spec ~width:5 in
+  let j_idx  = Variable.reg spec ~width:5 in
 
   let ax = Variable.reg spec ~width:32 in
   let ay = Variable.reg spec ~width:32 in
@@ -109,8 +124,25 @@ let algo ~clock ~clear ~load_finished ~(word_count : Signal.t) ~(read_word : Sig
   let by = Variable.reg spec ~width:32 in
   let bz = Variable.reg spec ~width:32 in
 
-  let min_d2 = Variable.reg spec ~width:64 in
-  let max_d2 = Variable.reg spec ~width:64 in
+  (* ===== Top-16 edges ===== *)
+  let best_d2 = Array.init 16 ~f:(fun _ -> Variable.reg spec ~width:64) in
+  let best_u  = Array.init 16 ~f:(fun _ -> Variable.reg spec ~width:5) in
+  let best_v  = Array.init 16 ~f:(fun _ -> Variable.reg spec ~width:5) in
+
+  (* ===== Union-find ===== *)
+  let parent = Array.init 20 ~f:(fun _ -> Variable.reg spec ~width:5) in
+  let size   = Array.init 20 ~f:(fun _ -> Variable.reg spec ~width:6) in
+  let unions_ok = Variable.reg spec ~width:6 in
+  let e_idx = Variable.reg spec ~width:4 in
+
+  let cur = Variable.reg spec ~width:5 in
+  let ru  = Variable.reg spec ~width:5 in
+  let rv  = Variable.reg spec ~width:5 in
+
+  let top1 = Variable.reg spec ~width:6 in
+  let top2 = Variable.reg spec ~width:6 in
+  let top3 = Variable.reg spec ~width:6 in
+  let sweep_i = Variable.reg spec ~width:5 in
 
   let done_fired = Variable.reg spec ~width:1 in
   let done_pulse = sm.is Done &: ~:(done_fired.value) in
@@ -131,17 +163,66 @@ let algo ~clock ~clear ~load_finished ~(word_count : Signal.t) ~(read_word : Sig
     +: sq64 (abs32 az.value bz.value)
   in
 
+  let bd = Array.map best_d2 ~f:(fun v -> v.value) in
+  let bu = Array.map best_u  ~f:(fun v -> v.value) in
+  let bv = Array.map best_v  ~f:(fun v -> v.value) in
+
+  let insert_at k =
+    let shifts =
+      List.concat
+        (List.init (15 - k) ~f:(fun t ->
+           [ best_d2.(k+t+1) <-- bd.(k+t)
+           ; best_u.(k+t+1)  <-- bu.(k+t)
+           ; best_v.(k+t+1)  <-- bv.(k+t)
+           ]))
+    in
+    shifts @
+    [ best_d2.(k) <-- d2
+    ; best_u.(k)  <-- i_idx.value
+    ; best_v.(k)  <-- j_idx.value
+    ]
+  in
+
+  let guards =
+    Array.init 16 ~f:(fun k ->
+      if k = 0 then d2 <: bd.(0)
+      else (d2 >=: bd.(k-1)) &: (d2 <: bd.(k)))
+  in
+
+  let read_rf rf idx = mux idx (Array.to_list rf) in
+  let parent_vals = Array.map parent ~f:(fun v -> v.value) in
+  let size_vals   = Array.map size   ~f:(fun v -> v.value) in
+
+  let p_cur = read_rf parent_vals cur.value in
+  let s_ru  = read_rf size_vals   ru.value in
+  let s_rv  = read_rf size_vals   rv.value in
+
+  let edge_u = read_rf (Array.map best_u ~f:(fun v -> v.value)) e_idx.value in
+  let edge_v = read_rf (Array.map best_v ~f:(fun v -> v.value)) e_idx.value in
+
+  let product_top3 =
+    uresize ~width:60
+      ((uresize ~width:60 top1.value *:
+        uresize ~width:60 top2.value) *:
+        uresize ~width:60 top3.value)
+  in
+
   compile
     [ sm.switch
-        [ (Loading, [ when_ load_finished [ sm.set_next Init ] ])
+        [ (Loading, [ when_ load_finished [ sm.set_next Init_scan ] ])
 
-        ; (Init,
+        ; (Init_scan,
             [ base_i <--. 0
             ; base_j <--. 3
-            ; min_d2 <-- ones 64
-            ; max_d2 <-- zero 64
+            ; i_idx  <--. 0
+            ; j_idx  <--. 1
             ; done_fired <-- gnd
-            ; sm.set_next Addr_ax ])
+            ]
+            @ List.concat (List.init 16 ~f:(fun k ->
+                [ best_d2.(k) <-- ones 64
+                ; best_u.(k)  <--. 0
+                ; best_v.(k)  <--. 0 ]))
+            @ [ sm.set_next Addr_ax ])
 
         ; (Addr_ax, [ addr <-- base_i.value; sm.set_next Wait_ax ])
         ; (Wait_ax, [ sm.set_next Consume_ax ])
@@ -165,32 +246,110 @@ let algo ~clock ~clear ~load_finished ~(word_count : Signal.t) ~(read_word : Sig
 
         ; (Addr_bz, [ addr <-- base_j.value +:. 2; sm.set_next Wait_bz ])
         ; (Wait_bz, [ sm.set_next Consume_bz ])
-        ; (Consume_bz, [ bz <-- read_word; sm.set_next Compute ])
+        ; (Consume_bz, [ bz <-- read_word; sm.set_next Compute_edge ])
 
-        ; (Compute,
-            [ when_ (d2 <: min_d2.value) [ min_d2 <-- d2 ]
-            ; when_ (d2 >: max_d2.value) [ max_d2 <-- d2 ]
-            ; sm.set_next Advance_j ])
+        ; (Compute_edge,
+            List.concat (List.init 16 ~f:(fun k ->
+              [ when_ guards.(k) (insert_at k) ]))
+            @ [ sm.set_next Advance_j ])
 
         ; (Advance_j,
             [ base_j <-- base_j.value +:. 3
+            ; j_idx  <-- j_idx.value  +:. 1
             ; if_ ((base_j.value +:. 3) >=: word_count)
                 [ sm.set_next Advance_i ]
                 [ sm.set_next Addr_bx ] ])
 
         ; (Advance_i,
             [ base_i <-- base_i.value +:. 3
+            ; i_idx  <-- i_idx.value  +:. 1
             ; base_j <-- base_i.value +:. 6
+            ; j_idx  <-- i_idx.value  +:. 2
             ; if_ ((base_i.value +:. 6) >=: word_count)
-                [ sm.set_next Done ]
+                [ sm.set_next Init_uf ]
                 [ sm.set_next Addr_ax ] ])
 
-        ; (Done,
-            [ when_ (done_fired.value ==:. 0) [ done_fired <-- vdd ] ])
-        ]
-    ];
+        ; (Init_uf,
+            [ unions_ok <--. 0
+            ; e_idx     <--. 0
+            ; top1 <--. 0; top2 <--. 0; top3 <--. 0
+            ; sweep_i <--. 0 ]
+            @ List.concat (List.init 20 ~f:(fun i ->
+                [ parent.(i) <--. i; size.(i) <--. 1 ]))
+            @ [ sm.set_next Apply_load ])
 
-  addr.value, min_d2.value, max_d2.value, done_pulse
+        ; (Apply_load, [ cur <-- edge_u; sm.set_next FindU_step ])
+
+        ; (FindU_step,
+            [ if_ (p_cur ==: cur.value)
+                [ ru <-- cur.value; cur <-- edge_v; sm.set_next FindV_step ]
+                [ cur <-- p_cur; sm.set_next FindU_step ] ])
+
+        ; (FindV_step,
+            [ if_ (p_cur ==: cur.value)
+                [ rv <-- cur.value; sm.set_next Do_union ]
+                [ cur <-- p_cur; sm.set_next FindV_step ] ])
+
+        ; (Do_union,
+            let attach_rv_to_ru =
+              List.concat
+                [ List.init 20 ~f:(fun n ->
+                    when_ (rv.value ==:. n) [ parent.(n) <-- ru.value ])
+                ; List.init 20 ~f:(fun n ->
+                    when_ (ru.value ==:. n) [ size.(n) <-- (s_ru +: s_rv) ])
+                ]
+            in
+            let attach_ru_to_rv =
+              List.concat
+                [ List.init 20 ~f:(fun n ->
+                    when_ (ru.value ==:. n) [ parent.(n) <-- rv.value ])
+                ; List.init 20 ~f:(fun n ->
+                    when_ (rv.value ==:. n) [ size.(n) <-- (s_ru +: s_rv) ])
+                ]
+            in
+            [
+              when_ (ru.value <>: rv.value)
+                [
+                  unions_ok <-- unions_ok.value +:. 1;
+                  if_ (s_ru >=: s_rv)
+                    attach_rv_to_ru
+                    attach_ru_to_rv
+                ];
+              sm.set_next Next_edge
+            ])
+
+
+        ; (Next_edge,
+            [ if_ (e_idx.value ==:. 9)
+                [ sm.set_next Sweep_init ]
+                [ e_idx <-- e_idx.value +:. 1
+                ; sm.set_next Apply_load ] ])
+
+        ; (Sweep_init,
+            [ sweep_i <--. 0; top1 <--. 0; top2 <--. 0; top3 <--. 0
+            ; sm.set_next Sweep_step ])
+
+        ; (Sweep_step,
+            let i = sweep_i.value in
+            let is_root = (read_rf parent_vals i ==: i) in
+            let s = mux2 is_root (read_rf size_vals i) (zero 6) in
+            [ when_ (s >: top1.value)
+                [ top3 <-- top2.value; top2 <-- top1.value; top1 <-- s ]
+            ; when_ ((s <=: top1.value) &: (s >: top2.value))
+                [ top3 <-- top2.value; top2 <-- s ]
+            ; when_ ((s <=: top2.value) &: (s >: top3.value))
+                [ top3 <-- s ]
+            ; if_ (i ==:. 19)
+                [ sm.set_next Done ]
+                [ sweep_i <-- i +:. 1; sm.set_next Sweep_step ] ])
+
+        ; (Done, [ when_ (~:(done_fired.value)) [ done_fired <-- vdd ] ])
+        ] ];
+
+  addr.value,
+  product_top3,
+  uresize ~width:60 unions_ok.value,
+  done_pulse
 ;;
 
 (* ====================== TOP ====================== *)
